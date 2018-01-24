ad08da3c8bf50aede3f4d7abe6faf3eb481ee8d5
LeBlanc7 <35480983+LeBlanc7@users.noreply.github.com>
2018-01-24 19:37:28 +0900

    Add files via upload

--- test.php
+++ test.php
@@ -1,24 +1,7 @@
 <? php
     if(!empty($_POST['song_name'])){
  
-        $db_host = "localhost";
-        $db_user = "root";
-        $db_pw = "111111";
-        $db_name = "tutorials";
-        $connect = mysqli_connect($db_host,$db_user,$db_pw,$db_name);
-
-        if(mysqli_connect_errno()){
-            printf("Connect failed : %s\n",mysqli_connect_error());
-            exit();
-        }
-
-        $query = "SELECT song_url FROM info WHERE song_name = {$_POST['song_name']}";
-        if(result = mysqli_query($connect,$query)) {
-            $url = mysqli_fetch_row($result);
-        }
-        
-        mysqli_free_result($result);
-        mysqli_close($connect);
+        $url = "http";
 
         echo $url;
     }
