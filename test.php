615bc52e680c9c54efc0fad5d0191b1bb328fd25
LeBlanc7 <35480983+LeBlanc7@users.noreply.github.com>
2018-01-25 15:56:22 +0900

    Add files via upload

--- test.php
+++ test.php
@@ -1,23 +1,24 @@
-<? php
-    if(!empty($_GET['song_name'])){
+<?php
+    
+    $song_name = $_GET['song_name'];
 
-        $db_host = "localhost";
-        $db_user = "root";
-        $db_pw = "111111";
-        $db_name = "tutorials";
-        $connect = mysqli_connect($db_host,$db_user,$db_pw,$db_name);
+    $db_host = "localhost";
+    $db_user = "root";
+    $db_pw = "111111";
+    $db_name = "tutorials";
+    $connect = mysqli_connect($db_host,$db_user,$db_pw,$db_name);
 
-        if(mysqli_connect_errno()){
-            printf("Connect failed : %s\n",mysqli_connect_error());
-            exit();
-        }
-
-        $query = "SELECT song_url FROM info WHERE song_name = {$_GET['song_name']}";
-        if(result = mysqli_query($connect,$query)) {
-            $url = mysqli_fetch_row($result);
-            $data = url[0];
-        }
-
-        echo $data;
+    if(mysqli_connect_errno()){
+        printf("Connect failed : %s\n",mysqli_connect_error());
+        exit();
     }
+
+    $query = "SELECT song_url FROM info WHERE song_name = '$song_name'";
+    if($result = mysqli_query($connect,$query)) {
+        $url = mysqli_fetch_row($result);
+        $data = $url[0];
+    }
+
+    echo $data;
+
 ?>
