29310ad6b84684ac410455e6edc93dc58cd590bd
LeBlanc7 <35480983+LeBlanc7@users.noreply.github.com>
2018-01-29 12:00:16 +0900

    Add files via upload

---
+++ search_song.php
@@ -0,0 +1,17 @@
+<?php
+
+    $song_name = $_GET['song'];
+
+    include $_SERVER['DOCUMENT_ROOT'].'/dbconnect.php';
+
+    /*$query = "SELECT song_artist,song_url FROM info WHERE song_name = '$song_name'";
+    if($result = mysqli_query($connect,$query)) {
+       
+
+    }*/
+
+    echo("<script>location.replace('test2.html');</script>");
+
+   
+
+?>
