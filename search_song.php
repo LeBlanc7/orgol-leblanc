3fbb4ecf976fc005a4f46a5a58fcc153490cfa89
LeBlanc7 <35480983+LeBlanc7@users.noreply.github.com>
2018-02-01 14:25:29 +0900

    Add files via upload

--- search_song.php
+++ search_song.php
@@ -4,12 +4,18 @@
 
     include $_SERVER['DOCUMENT_ROOT'].'/dbconnect.php';
 
-    /*$query = "SELECT song_artist,song_url FROM info WHERE song_name = '$song_name'";
+    $query = "SELECT song_artist,song_name,song_url,song_category FROM info WHERE song_artist like '%$song_name%'";
     if($result = mysqli_query($connect,$query)) {
-       
+       $song = mysqli_fetch_row($result);
+       if(!$song) echo "<h1> no song in DB</h1>";
+       else {
+           while($song){
+                echo "<p>artist is $song[0]</p>";
+                echo "<p>name is $song[1]</p>";
+                echo "<p>url is $song[2]</p>";
+           }
+       }
 
-    }*/
-
-    echo "<script>viewSearchResult();</script>";
+    }
 
 ?>
