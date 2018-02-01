b7ce5f63c18ba76025bc2ef8335fcf1809c47303
LeBlanc7 <35480983+LeBlanc7@users.noreply.github.com>
2018-02-01 15:24:43 +0900

    Add files via upload

--- search_song.php
+++ search_song.php
@@ -6,12 +6,13 @@
 
     $query = "SELECT song_artist,song_name,song_url,song_category FROM info WHERE song_artist like '%$song_name%'";
     if($result = mysqli_query($connect,$query)) {
-       while($song = mysqli_fetch_row($result)) {
-            if(!$song) echo "<h1> no song in DB</h1>";
-            else {
-                    echo "<p>artist is $song[0]</p>";
-                    echo "<p>name is $song[1]</p>";
-                    echo "<p>url is $song[2]</p>";         
+        if(!$song) echo "<h1> no song in DB</h1>";
+        else {
+            while($song = mysqli_fetch_row($result)) {
+                echo "<h1> $song[0] - $song[1]</h1>";
+                echo "<iframe width='680' height='360' src='https://www.youtube.com/embed/$song[2]' frameborder='0' allowfullscreen></iframe>";
+                echo "<br><br> " ;
+                echo "<a href='https://www.youtube.com/watch?v=$song[2] 'target='_blank'>Youtube에서 보기</a>"        
             }
         }
     }
