d2a2ccd239e9680c103a8d45b8c789385b9f6bed
LeBlanc7 <35480983+LeBlanc7@users.noreply.github.com>
2018-02-12 16:45:49 +0900

    Add files via upload

--- get_song_recommend.php
+++ get_song_recommend.php
@@ -28,24 +28,24 @@
             }
         }
         for($i = 0; $i < 3; $i++){
-            echo "<div class='grid'>";
-            echo "<div class='image'><a href='./main.php?id=".$data[$arr[$i]]['no']."'>";
+            echo "<div class='wrap'>";
+            echo "<div class='image'><a href='./test2.html?id=".$data[$arr[$i]]['no']."'>";
             echo "<img src='https://img.youtube.com/vi/".$data[$arr[$i]]['url']."/default.jpg' class='img' name='".$data[$arr[$i]]['name']."'></a></div>";
-            echo "<div class='image'><a href='./main.php?id=".$data[$arr[$i]]['no']."'>".$data[$arr[$i]]['artist']." - ".$data[$arr[$i]]['name']."</a>";
+            echo "<div class='image'><a href='./test2.html?id=".$data[$arr[$i]]['no']."'>".$data[$arr[$i]]['artist']." - ".$data[$arr[$i]]['name']."</a>";
             echo "<hr size='1'>";
-            echo "<a href='https://www.youtube.com/watch?v=".$data[$arr[$i]]['url']."'target='_blank'>Youtube에서 보기</a></div></div><br><br>";
+            echo "<a href='https://www.youtube.com/watch?v=".$data[$arr[$i]]['url']."'target='_blank'>Youtube에서 보기</a></div></div>";
             }
         }
         else if(count($data)<4 && count($data)>1){
             for($i = 0; $i < count($data); $i++){
                 if($data[$i]['name'] == $temp['name'])
                 continue;
-                echo "<div class='grid'>";
-                echo "<div class='image'><a href='./main.php?id=".$data[$i]['no']."'>";
+                echo "<div class='wrap'>";
+                echo "<div class='image'><a href='./test2.html?id=".$data[$i]['no']."'>";
                 echo "<img src='https://img.youtube.com/vi/".$data[$i]['url']."/default.jpg' class='img' name='".$data[$i]['name']."'></a></div>";
-                echo "<div class='image'><a href='./main.php?id=".$data[$i]['no']."'>".$data[$i]['artist']." - ".$data[$i]['name']."</a>";
+                echo "<div class='image'><a href='./test2.html?id=".$data[$i]['no']."'>".$data[$i]['artist']." - ".$data[$i]['name']."</a>";
                 echo "<hr size='1'>";
-                echo "<a href='https://www.youtube.com/watch?v=".$data[$i]['url']."'target='_blank'>Youtube에서 보기</a></div></div><br><br>";
+                echo "<a href='https://www.youtube.com/watch?v=".$data[$i]['url']."'target='_blank'>Youtube에서 보기</a></div></div>";
             }
     }
 ?>
